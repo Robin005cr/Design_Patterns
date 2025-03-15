a259640f2b8d03147676f899ec30fd5940bf2df7
Robin CR <robin005cr@gmail.com>
2025-03-15 22:57:16 +0530

    Adding comments to the files

--- Interfaces1.cpp
+++ Interfaces1.cpp
@@ -1,4 +1,17 @@
 /*
+* project   : https://github.com/Robin005cr/Design_Patterns
+* file name : ArrayOFObject.cpp
+* author    : Robin CR
+* mail id   : robinchovallurraju@gmail.com
+* LinkedIn  : https://www.linkedin.com/in/robin-cr/
+* portfolio : https://robin005cr.github.io/
+*
+* Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+* Suggestions for improvements or better methods are always welcome and appreciated.
+* I value constructive feedback and aim to continuously improve the quality of the work.
+*
+*/
+/*
 Scenario: Here we are creating a pointer variant1 of type IEngine.This allows the program to handle
 different types of engines(Diesel or Petrol) through the same interface.
 
